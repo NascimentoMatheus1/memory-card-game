1e13e30e366429040ce4ffe263b5029c502bec5e
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-01-06 20:29:09 -0300

    Feat: Add the basic structure of the game board and cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import Card from './components/Card';
+
 function App() {
     return (
         <>
-            <p>hello world</p>
+            <header>
+                <div>
+                    <h1 className="game-title">Dragon Ball Z Memory Game</h1>
+                </div>
+                <div className="scores-container">
+                    <p className="score">Score: 0</p>
+                    <p className="best-score">Best Score: 0</p>
+                </div>
+            </header>
+            <main>
+                <p>
+                    Get points by clicking on an image but don't click on any
+                    more than once!
+                </p>
+                <div className="board">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+            </main>
         </>
     );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+function Card() {
+    return (
+        <div className="card">
+            <img
+                className="card-image"
+                src="https://dragonball-api.com/characters/gohan.webp"
+                alt="character image"
+            />
+            <h2 className="card-title">Gohan</h2>
+        </div>
+    );
+}
+
+export default Card;
